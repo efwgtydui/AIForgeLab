eeb30dfe4bf3ad7b4f729143346ef8aa22b9f139
efwgtydui <msjci.lupinetti@hotmail.com>
2024-05-13 08:14:34 +0700

    start project

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
-  }
-  return false;
-}
+const titleCase = (str) =>
+  str.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
